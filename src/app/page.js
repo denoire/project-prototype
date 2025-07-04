ff0048071a0eb8886557d48d2abeb7e44b5aab6a
denoire <thomaszac19@gmail.com>
2025-07-03 17:53:34 -0700

    Set up initial Next.js file structure with pages, components, and imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
-import Image from "next/image";
+import NavBar from '@/app/components/NavBar';
+import IssueFinder from "@/app/issue-finder/page";
+import ReadmeViewer from "@/app/readme-viewer/page";
+import ProfileComparison from "@/app/profile-comparison/page";
+import MarkdownGenerator from "@/app/markdown-generator/page";
 
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+        </>
+    );
 }
